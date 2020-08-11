d9e37a394e1c3e97c3f21f955f8e5d6585f8e0d8
Ningayya <ningayya777@gmail.com>
2020-08-11 18:30:47 +0530

    UserProfile verification changes

--- src/main/java/com/modestack/controllers/ModestackController.java
+++ src/main/java/com/modestack/controllers/ModestackController.java
@@ -31,9 +31,14 @@ public class ModestackController {
 	private ResponseEntity<Object> userRegistory(@RequestBody UserProfile userProfile) {
 
 		UserProfile profile = modestackService.userRegistory(userProfile);
+		
+		if(profile==null) {
 
-		return new ResponseEntity<Object>("new user created",HttpStatus.CREATED);
-
+			return new ResponseEntity<Object>("User name already exists,choose diffrent user name",HttpStatus.CONFLICT);
+		}
+		
+		
+		else	return new ResponseEntity<Object>("new user created",HttpStatus.CREATED);
 	}
 
 	@ApiOperation(httpMethod = "POST", value = "login success.")
